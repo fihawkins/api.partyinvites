76833c711956c250d7b9aebf540e18418192af78
engageflopes <filipe.lopes@engage.bz>
2017-12-11 15:58:48 -0200

    chapter 2

---
+++ PartyInvites/Controllers/RsvpController.cs
@@ -0,0 +1,23 @@
+﻿using PartyInvites.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace PartyInvites.Controllers
+{
+    public class RsvpController : ApiController
+    {
+        public IEnumerable<GuestResponse> GetAttendees()
+        {
+            return Repository.Responses.Where(x => x.WillAttend == true);
+        }
+
+        public void PostResponse(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.Add(response);
+            }
+        }
+    }
+}
